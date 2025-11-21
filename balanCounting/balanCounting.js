78c12483f8a686e7513d59639ab406698ee605b1
Alexis Pantoja <pantojariosalexisjair@gmail.com>
2025-11-21 15:06:26 -0500

    Add files via upload

---
+++ balanCounting/balanCounting.js
@@ -0,0 +1,35 @@
+// Función para actualizar el logo según la posición del scroll
+function actualizarLogo() {
+  const header = document.getElementById("main-header");
+  const logo = document.getElementById("logo");
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 160) {
+    header.classList.add("scrolled");
+    logo.src = "../static/img/balanCounting/balan_services_counting_2.png";
+  } else {
+    header.classList.remove("scrolled");
+    logo.src = "../static/img/balanCounting/balan_services_counting_2.png";
+  }
+}
+
+// Ejecutar al hacer scroll
+window.addEventListener("scroll", actualizarLogo);
+
+// Ejecutar también al cargar la página
+window.addEventListener("DOMContentLoaded", actualizarLogo);
+
+// Animación al hacer scroll
+window.addEventListener('scroll', () => {
+  const areas = document.querySelectorAll('.area');
+  const triggerBottom = window.innerHeight * 0.85;
+
+  areas.forEach(area => {
+    const areaTop = area.getBoundingClientRect().top;
+    if (areaTop < triggerBottom) {
+      area.classList.add('visible');
+    }
+  });
+});
+
+
