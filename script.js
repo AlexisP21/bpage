48a761c942150b38a8107d924cf3fb42d316c220
Alexis Pantoja <pantojariosalexisjair@gmail.com>
2025-12-17 13:48:43 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ window.addEventListener('scroll', () => {
     const areaTop = area.getBoundingClientRect().top;
     if (areaTop < triggerBottom) {
       area.classList.add('visible');
-    }
-  });
+    }
+  });
 });
 
 // --- Menú hamburguesa ---
@@ -37,8 +37,51 @@ const hamburger = document.getElementById('hamburger');
 const menuMobile = document.getElementById('menuMobile');
 
 hamburger.addEventListener('click', () => {
-    menuMobile.classList.toggle('active');
+  menuMobile.classList.toggle('active');
 });
+
+
+
+
+// ----- Popup Minimizar -----
+const popup = document.getElementById("popup-container");
+const minimizedBtn = document.getElementById("minimized-btn");
+const closePopup = document.getElementById("closeBtn");
+
+closePopup.addEventListener("click", () => {
+  popup.style.display = "none";
+  minimizedBtn.style.display = "flex";
+});
+
+minimizedBtn.addEventListener("click", () => {
+  popup.style.display = "flex";
+  minimizedBtn.style.display = "none";
+});
+
+// ----- RELOJ 24 HORAS -----
+function startCountdown(duration, displayMain, displayMini) {
+  let timer = duration;
+
+  setInterval(() => {
+    let hours = String(Math.floor(timer / 3600)).padStart(2, "0");
+    let minutes = String(Math.floor((timer % 3600) / 60)).padStart(2, "0");
+    let seconds = String(timer % 60).padStart(2, "0");
+
+    displayMain.textContent = `${hours}:${minutes}:${seconds}`;
+    displayMini.textContent = `${hours}:${minutes}:${seconds}`;
+
+    if (--timer < 0) timer = 0;
+  }, 1000);
+}
+
+window.onload = () => {
+  const countdownMain = document.getElementById("countdown");
+  const countdownMini = document.getElementById("mini-countdown");
+
+  const duration = 24 * 60 * 60; // 24 horas en segundos
+
+  startCountdown(duration, countdownMain, countdownMini);
+};
 
 
 
